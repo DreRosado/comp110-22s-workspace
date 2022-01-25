240f9e172d1bd96ca1de73f7e2124266b37bfc5d
Andre <andre.rosado@unc.edu>
2022-01-24 23:35:45 -0500

    Progress on input project

---
+++ lessons/input.py
@@ -0,0 +1,8 @@
+"""Demonstrates asking user for input"""
+___author___ = "730524237"
+
+user_name: str = input("What is your name? ")
+print("Hello, " + user_name + ", good morning!")
+print("Put on your seatbelts!! We're logging you in now...")
+print("Just to double check... Your name is, " + user_name + ".")
+input("Correct?")
